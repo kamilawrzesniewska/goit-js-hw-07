c8a83ce1cd8aae5fe56be380463f6d175d75a611
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-07-25 08:38:42 +0200

    task 2 updated

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList =  document.querySelector(".gallery");
+
+
+
+const galleryItemsElement = galleryItems.map(element => {
+            const image = `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}" >
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`;
+      return image;
+      })
+      .join(" ");
+
+      
+galleryList.insertAdjacentHTML("beforeend", galleryItemsElement);  
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionClass: "bottom",
+    captionDelay: "250",
+});
 
